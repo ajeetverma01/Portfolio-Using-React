36268f91135b851179b437204e92547063d5f792
Ajeet Verma <av401402@gmail.com>
2025-04-04 12:10:28 +0530

    Fixing bug

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./CSS/about.css";
-import myPhoto from "../assets/myphoto.jpg"; // Replace with your actual image
+import myPhoto from "../assets/myPhoto.jpg"; // Replace with your actual image
 import { Link } from "react-router-dom"; // For navigation
 
 const About = () => {
